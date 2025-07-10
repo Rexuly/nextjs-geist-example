e69d0df6f14afa1465207fe527ee8df9132d397c
Rexuly <104594084+Rexuly@users.noreply.github.com>
2025-07-10 19:15:13 -0400

    Add .next/static/chunks/src_app_page_tsx_9148afbf._.js 1752189312249

---
+++ .next/static/chunks/src_app_page_tsx_9148afbf._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_page_tsx_b025fed5._.js",
+  "static/chunks/node_modules_next_dist_compiled_react_26ec58f1._.js",
+  "static/chunks/src_app_quagen-menu_cc3b5d0e.css"
+],
+    source: "dynamic"
+});
